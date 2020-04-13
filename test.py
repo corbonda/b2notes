f662b8c8ac8ce5feb5f8a8009aca355056bc411c
Cor Bonda <cor.bonda@axians.com>
2020-04-14 00:05:51 +0200

    Cleaned first file and cleaned requirement list
    Signed-off-by: Cor Bonda <cor.bonda@axians.com>

--- test.py
+++ test.py
@@ -1,48 +1,33 @@
-import wx
 import uuid
-
-app = wx.App(False)  # Create a new app, don't redirect stdout/stderr to a window.
-frame = wx.Frame(None, wx.ID_ANY, "Hello World") # A Frame is a top-level window.
-frame.Show(True)     # Show the frame.
-app.MainLoop()
-
-
-brainlist = ["sasd","lkjlk"]
-
-
+## NOTE CLASS ##
 class note:
   def __init__(self, text, link=None):
     self.text = text
     self.ID = uuid.uuid4().hex
     self.link = link
+  # def append(obj):
+  #   if isinstance(self, list):
+  #    obj = .append(note1.ID)
+  #   else:
+  #     note0.link = list(note1.ID)
+  #   note1.link = note0.ID 
 
-def link(note0, note2):
-    note0.link = note1.ID
-    note1.link = note0.ID
-
-
-def link(note0, note1=None):
-    print(len(note0))
-    if len(note0) > 1:
-        i = 0
-        while i < len(note0):
-            print(i)
-            i += 1
-    else:
-        note0.link = note1.ID
-        note1.link = note0.ID
 
     
+# del note1
 note1 = note(text="onzin")
 note2 = note('mooie dingen')
 
-link(note1, note2)
- 
+
 print(note1.text)
+print(note1.ID)
 print(note1.link)
 
 
 print(note2.text)
+print(note2.ID)
 print(note2.link)
+
+
 
 
